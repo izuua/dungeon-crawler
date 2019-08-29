511f43f276b44aa755b85f6ee45f6103d650e0f7
izuua <izuuaaf@gmail.com>
2019-08-29 11:20:23 -0400

    Added the react app.

---
+++ src/features/player/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import walkSprite from './player_walk.png';
+import handleMovement from './movement';
+
+function Player(props) {
+    return (
+        <div
+        style={{
+            position: 'absolute',
+            top: props.position[1],
+            left: props.position[0],
+            backgroundImage: `url('${walkSprite}')`,
+            backgroundPosition: '0 0',
+            width: '40px',
+            height: '40px',
+        }}
+        />
+    )
+}
+
+function mapStateToProps(state) {
+    return {
+        ...state.player,
+    }
+}
+
+function getState(fn1, fn2) {
+    return function(component) {
+        return component;
+    }
+}
+
+export default connect(mapStateToProps)(handleMovement(Player)); 
