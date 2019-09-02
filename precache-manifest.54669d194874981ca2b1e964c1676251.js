2f88d1ca06d13233f641bd627f3c62a8e1f47964
izuua <izuuaaf@gmail.com>
2019-09-02 17:53:13 -0400

    Updates

---
+++ precache-manifest.54669d194874981ca2b1e964c1676251.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af37fdc10aa2c92d77720dadd145ca8b",
+    "url": "/dungeon-crawler/index.html"
+  },
+  {
+    "revision": "b90903dc56224d409cd3",
+    "url": "/dungeon-crawler/static/css/main.97742322.chunk.css"
+  },
+  {
+    "revision": "db1d0b8f82477148848b",
+    "url": "/dungeon-crawler/static/js/2.58cad70b.chunk.js"
+  },
+  {
+    "revision": "b90903dc56224d409cd3",
+    "url": "/dungeon-crawler/static/js/main.a6eed437.chunk.js"
+  },
+  {
+    "revision": "869bcdf1209bc6703bf2",
+    "url": "/dungeon-crawler/static/js/runtime~main.3c8130e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dungeon-crawler/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9f19440f74f1c8126fdcf003e3d024da",
+    "url": "/dungeon-crawler/static/media/player_walk.9f19440f.png"
+  }
+]);
